f52b286406d52ba11b78a524ee85be94b17e1212
wtw <wtwmiao@qq.com>
2017-05-11 18:02:59 +0800

    Commit

--- copy_from_local.sh
+++ copy_from_local.sh
@@ -10,4 +10,4 @@ echo "will copy user profile"
 cp  -rpf ~/Library/Developer/Xcode/UserData/Ke*    "$(pwd)/backup_use_shell/"
 cp  -rpf ~/Library/Developer/Xcode/UserData/Fo*    "$(pwd)/backup_use_shell/"
 
-
+cp  -rpf ~/Library/Developer/Xcode/UserData/IDEPref*    "$(pwd)/backup_use_shell/"
